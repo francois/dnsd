4a4866fda4203ea86a747e8e34a7d3e247558209
François Beausoleil <francois@teksol.info>
2014-03-23 14:08:16 -0400

    Open socket (transitory state)

--- boot.c
+++ boot.c
@@ -1,4 +1,5 @@
 #include "dnsd.h"
+#include <sys/socket.h>
 
 server_state* new_dnsd(int argc, char** argv) {
 	int i;
@@ -9,14 +10,33 @@ server_state* new_dnsd(int argc, char** argv) {
 
 	server_state* sstate;
 	sstate = calloc(1, sizeof(server_state));
+	if( !sstate ) return sstate;
+
+	// assign defaults
+	sstate->port = 2353;
+
+	// parse options and assign values
+
 	return sstate;
 }
 
 void init(server_state* sstate) {
+	assert( sstate );
+
 	// read zone file / data to serve
-	// open socket
+
+	LOG("Opening socket on port %d\n", sstate->port);
+	sstate->socket = socket( PF_INET, SOCK_DGRAM, -1 );
+	if( !sstate->socket ) {
+		LOG2("Failed to open socket on port %d (errno: %d)\n", sstate->port, errno);
+		free_dnsd(sstate);
+		exit(2);
+	}
 }
 
 void free_dnsd(server_state* sstate) {
+	assert( sstate );
+
+	close(sstate->socket);
 	free(sstate);
 }
--- run.c
+++ run.c
@@ -1,6 +1,8 @@
 #include "dnsd.h"
 
 void run(server_state* sstate) {
+	assert( sstate );
+
 	// while accept
 	//	serve
 }
